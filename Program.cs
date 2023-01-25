1274cc803b7d027e59d9ecc7d68a9825479bcfff
Dima <eliseev.rds@gmail.com>
2023-01-26 00:09:02 +0300

    Введите второе число

--- Program.cs
+++ Program.cs
@@ -1,3 +1,6 @@
 ﻿int max = 0;
 int min = 0;
-
+Console.Write("Введите первое число: ");
+int firstNumber = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите второе число: ");
+int secondNumber = Convert.ToInt32(Console.ReadLine());
