8442552eb23bc6200f0b2ab1c3da9778c9f9c923
Dima <eliseev.rds@gmail.com>
2023-01-26 00:10:17 +0300

    добавил сравнения для решенияо

--- Program.cs
+++ Program.cs
@@ -4,3 +4,13 @@ Console.Write("Введите первое число: ");
 int firstNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите второе число: ");
 int secondNumber = Convert.ToInt32(Console.ReadLine());
+if(firstNumber > secondNumber)
+{
+    max = firstNumber;
+    min = secondNumber;
+}
+else
+{
+    max = secondNumber;
+    min = firstNumber;
+}
